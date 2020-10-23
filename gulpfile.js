66a2326c99fed7006487cc71698b2a8a6ade27dc
Luis Munoz <lmunoz.xyz@gmail.com>
2020-10-23 12:52:35 -0600

    HAsta aqui, todo bien, se instalaron mas tools py seguidamente voy a proceder al setup en el gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,18 +10,13 @@ var browserSync = require('browser-sync').create();
         done();
       });
 
-     
-
       gulp.task('sass:watch', function(done){
-
         // added done above too
     
         gulp.watch('./css/*.scss', gulp.series('sass') )
         browserSync = require('browser-sync');
         done();
     })
-
-
 
       gulp.task('browser-sync', done => {
         var files = ['./*.html','./css/*.css','./img/*.{png, jpg, gif}','./js/*.js'];
@@ -34,5 +29,4 @@ var browserSync = require('browser-sync').create();
         done();
       });
 
-   gulp.task('default', gulp.series(['sass', 'browser-sync', 'sass:watch']));
-
+   gulp.task('default', gulp.series(['sass', 'browser-sync', 'sass:watch']))
