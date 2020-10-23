e9756756186faa8c446224409cfda7770c6fed8c
Luis Munoz <lmunoz.xyz@gmail.com>
2020-10-23 11:37:06 -0600

    cambiamos el encabezado y al final y el gulp finalmente levanta y todo bien

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,34 @@
 'use strict'
+var gulp = require('gulp');
+var sass =  require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-var gulp = require('gulp'),
-sass =  require('gulp-sass');
+    gulp.task('sass', done => {
+        gulp.src('./css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+        done();
+      });
 
-  gulp.task('sass', done => {
-    gulp.src('./css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-    done();
-  });
+      gulp.task('sass:watch', done => {
+        gulp.watch('./css/*.scss/', ['sass']),
+        browserSync = require('browser-sync');
+        done();
+      });
+
+      gulp.task('browser-sync', done => {
+        var files = ['./*.html','./css/*.css','./img/*.{png, jpg, gif}','./js/*.js'];
+        browserSync.init(files, {
+            server: {
+                baseDir: './'
+                }
+            });
+        
+        done();
+      });
+
+
+
+
+    gulp.task('default', gulp.series(['sass', 'browser-sync']));
+
